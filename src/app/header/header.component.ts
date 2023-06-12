e1f3d7159b3828ba6a0bce534d5e98d7e3e28e4f
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-11 22:08:42 -0600

    cambios

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FirestoreService } from '../servicios/FirestoreListas.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(
+    private datos_Locales: FirestoreService,
+    private location: Location
+  ) {}
+
+  CerrarSesion() {
+    this.datos_Locales.Actualizar_Login(false);
+    this.datos_Locales.eliminarCacheNavegador();
+    this.datos_Locales.Actualizar_Formulario(false);
+    this.location.go('/Sistema/Registro');
+  }
 
 }
